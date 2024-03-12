0ca5324e705942658e44605d92217bd3ef08396b
Stokn9x <capper2704@hotmail.dk>
2024-03-12 10:41:09 +0100

    My python is fucking :(

---
+++ CoachingAPI/Python/Data.py
@@ -0,0 +1,15 @@
+
+killList = [23,22,12,12,30]
+
+def killFreq(kills):
+    var = sum(kills)
+    result = var / len(kills)
+    print(result)
+
+def Hello():
+    print("hej")
+    
+
+
+if __name__ == "__main__":
+    killFreq(killList)
---
+++ CoachingAPI/Python/PyConnect.cs
@@ -0,0 +1,22 @@
+﻿using Python.Runtime;
+namespace CoachingAPI.Python
+{
+    public class PyConnect
+    {
+
+        public static void RunScript(string scriptName)
+        {
+            Runtime.PythonDLL = @"C:\\Users\\cappe\\AppData\\Local\\Programs\\Python\\Python312.dll";
+            PythonEngine.Initialize();
+            using (Py.GIL())
+            {
+                var pythonScript = Py.Import(scriptName);
+                pythonScript.InvokeMethod("Hello");
+            }
+        }
+
+        RunScript("Data");
+
+
+    }
+}
